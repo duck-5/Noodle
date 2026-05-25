b14c2097931ce8e0c9dd97783e7d33924f4680e8
JesusDMan <mantin.work@gmail.com>
2026-05-25 18:10:38 +0300

    refactor: reorganize client modules into unified clients package

---
+++ clients/__init__.py
@@ -0,0 +1,3 @@
+from .moodle_client import get_enrolled_courses, get_pending_assignments, parse_course_metadata
+from .panopto_client import get_new_lectures
+from .google_client import get_google_services, sync_data
